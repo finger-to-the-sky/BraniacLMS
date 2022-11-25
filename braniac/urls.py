ef529043545361537062355bcae97bad9e6113c5
zagmak <mzagrebam@gmail.com>
2022-11-25 23:13:36 +0200

    add url in templates for  Homework Lesson_2

--- braniac/urls.py
+++ braniac/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('mainapp/', include('mainapp.urls')),
+    path('', RedirectView.as_view(url='mainapp/'))
+    path('mainapp/', include('mainapp.urls', namespace='mainapp')),
 ]
