a748e396b622d8afb8bb23ea692e2a63a303a56e
zagmak <mzagrebam@gmail.com>
2022-12-12 15:54:48 +0200

    add new app "authapp" and models "CustomUser"

---
+++ authapp/models.py
@@ -0,0 +1,63 @@
+from pathlib import Path
+from time import time
+
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin, UserManager
+from django.contrib.auth.validators import ASCIIUsernameValidator
+from django.core.mail import send_mail
+from django.db import models
+from django.utils.translation import gettext_lazy
+
+
+def users_avatars_path(instance, filename):
+    # file will be uploaded to
+    # MEDIA_ROOT / user_<username> / avatars / <filename>
+    num = int(time() * 1000)
+    suff = Path(filename).suffix
+    return f'user_{instance.username}/avatars/pic_{num}{suff}'
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    username_validator = ASCIIUsernameValidator()
+
+    username = models.CharField(gettext_lazy('username'), max_length=150, unique=True,
+                                help_text=gettext_lazy('Required. 150 characters of fewer. Letters, digits, '
+                                                       'and @/./+/-/_ only.'),
+                                validators=[username_validator],
+                                error_messages={"unique": gettext_lazy('A user with that username already exists')})
+    first_name = models.CharField(gettext_lazy('first name'), max_length=150, blank=True)
+    last_name = models.CharField(gettext_lazy('last name'), max_length=150, blank=True)
+    age = models.PositiveIntegerField(blank=True, null=True)
+    avatar = models.ImageField(upload_to=users_avatars_path, blank=True, null=True)
+    email = models.EmailField(gettext_lazy('email address'), unique=True, max_length=256, error_messages={
+        "unique": gettext_lazy('A user with that email already exists')
+    })
+    is_staff = models.BooleanField(gettext_lazy('staff status'), default=False,
+                                   help_text=gettext_lazy('Designates whether the user can log into this admin site'))
+    is_active = models.BooleanField(gettext_lazy('staff status'), default=True,
+                                    help_text=gettext_lazy('Designates whether this user should be treated as active.'
+                                                           'Unselect this instead of deleting accounts'))
+    date_joined = models.DateTimeField(gettext_lazy('date joined'), auto_now_add=True)
+    objects = UserManager()
+
+    EMAIL_FIELD = 'email'
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    class Meta:
+        verbose_name = gettext_lazy('user')
+        verbose_name_plural = gettext_lazy('users')
+
+    def clean(self):
+        super().clean()
+        self.email = self.__class__.objects.normalize_email(self.email)
+
+    def get_full_name(self):
+        full_name = "%s %s" % (self.first_name, self.last_name)
+        return full_name.strip()
+
+    def get_short_name(self):
+        return self.first_name
+
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        send_mail(subject, message, from_email, [self.email], **kwargs)
--- mainapp/models.py
+++ mainapp/models.py
@@ -18,14 +18,21 @@ class News(models.Model):
         self.save()
 
 
+class CoursesManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(deleted=False)
+
+
 class Courses(models.Model):
-    name = models.CharField(max_length=256, verbose_name='Name')
-    description = models.TextField(verbose_name='Description', blank=True, null=True)
-    description_as_markdown = models.BooleanField(verbose_name='As markdown', default=False)
-    cost = models.DecimalField(max_digits=8, decimal_places=2, verbose_name='Cost', default=0)
-    cover = models.CharField(max_length=25, default='no_image.svg', verbose_name='Cover')
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created', editable=False)
-    updated_at = models.DateTimeField(auto_now=True, verbose_name='Updated', editable=False)
+    objects = CoursesManager()
+
+    name = models.CharField(max_length=256, verbose_name="Name")
+    description = models.TextField(verbose_name="Description", blank=True, null=True)
+    description_as_markdown = models.BooleanField(verbose_name="As markdown", default=False)
+    cost = models.DecimalField(max_digits=8, decimal_places=2, verbose_name="Cost", default=0)
+    cover = models.CharField(max_length=25, default="no_image.svg", verbose_name="Cover")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Created")
+    updated = models.DateTimeField(auto_now=True, verbose_name="Edited")
     deleted = models.BooleanField(default=False)
 
     def __str__(self):
@@ -47,7 +54,7 @@ class Lesson(models.Model):
     deleted = models.BooleanField(default=False)
 
     def __str__(self):
-        return f'{self.course.name} | {self.num} | {self.title} '
+        return f'{self.course.name} | {self.num} | {self.title}'
 
     def delete(self, *args):
         self.deleted = True
