1bc6281f6ac7375415c10fa5582a35339f1b6dde
zagmak <mzagrebam@gmail.com>
2022-12-13 12:19:43 +0200

    lesson_5 Authapp done

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,6 +1,7 @@
-from django.views.generic import TemplateView
 from django.shortcuts import get_object_or_404
-from mainapp import models
+from django.views.generic import TemplateView
+
+from mainapp import models as mainapp_models
 
 
 class MainPageView(TemplateView):
@@ -11,16 +12,19 @@ class NewsPageView(TemplateView):
     template_name = "mainapp/news.html"
 
     def get_context_data(self, **kwargs):
+        # Get all previous data
         context = super().get_context_data(**kwargs)
-        context['news_qs'] = models.News.objects.all()[:5]
+        # Create your own data
+        context["news_qs"] = mainapp_models.News.objects.all()[:5]
         return context
 
 
 class NewsPageDetailView(TemplateView):
+    template_name = "mainapp/news_detail.html"
 
     def get_context_data(self, pk=None, **kwargs):
         context = super().get_context_data(pk=pk, **kwargs)
-        context['news_object'] = get_object_or_404(models.News, pk=pk)
+        context["news_object"] = get_object_or_404(mainapp_models.News, pk=pk)
         return context
 
 
@@ -29,7 +33,7 @@ class CoursesListView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(CoursesListView, self).get_context_data(**kwargs)
-        context["objects"] = models.Courses.objects.all()[:7]
+        context["objects"] = mainapp_models.Courses.objects.all()[:7]
         return context
 
 
@@ -38,9 +42,9 @@ class CoursesDetailView(TemplateView):
 
     def get_context_data(self, pk=None, **kwargs):
         context = super(CoursesDetailView, self).get_context_data(**kwargs)
-        context["course_object"] = get_object_or_404(models.Courses, pk=pk)
-        context["lessons"] = models.Lesson.objects.filter(course=context["course_object"])
-        context["teachers"] = models.CourseTeachers.objects.filter(course=context["course_object"])
+        context["course_object"] = get_object_or_404(mainapp_models.Courses, pk=pk)
+        context["lessons"] = mainapp_models.Lesson.objects.filter(course=context["course_object"])
+        context["teachers"] = mainapp_models.CourseTeachers.objects.filter(course=context["course_object"])
         return context
 
 
@@ -50,7 +54,3 @@ class ContactsPageView(TemplateView):
 
 class DocSitePageView(TemplateView):
     template_name = "mainapp/doc_site.html"
-
-
-class LoginPageView(TemplateView):
-    template_name = "mainapp/login.html"
