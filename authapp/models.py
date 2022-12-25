6a5fdeaea23256e0da16c4850e860e844e19cae4
zagmak <mzagrebam@gmail.com>
2022-12-25 11:25:24 +0200

    Refactoring several mistakes

--- authapp/models.py
+++ authapp/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import PermissionsMixin, UserManager
 from django.contrib.auth.validators import ASCIIUsernameValidator
 from django.core.mail import send_mail
 from django.db import models
-from django.utils.translation import gettext_lazy
+from django.utils.translation import gettext_lazy as _
 
 
 def users_avatars_path(instance, filename):
@@ -20,25 +20,25 @@ def users_avatars_path(instance, filename):
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     username_validator = ASCIIUsernameValidator()
 
-    username = models.CharField(gettext_lazy('username'), max_length=150, unique=True,
+    username = models.CharField(_('username'), max_length=150, unique=True,
                                 help_text=_(
                                     "Required. 150 characters or fewer. ASCII letters and digits only."
                                 ),
                                 validators=[username_validator],
-                                error_messages={"unique": gettext_lazy('A user with that username already exists')})
-    first_name = models.CharField(gettext_lazy('first name'), max_length=150, blank=True)
-    last_name = models.CharField(gettext_lazy('last name'), max_length=150, blank=True)
+                                error_messages={"unique": _('A user with that username already exists')})
+    first_name = models.CharField(_('first name'), max_length=150, blank=True)
+    last_name = models.CharField(_('last name'), max_length=150, blank=True)
     age = models.PositiveIntegerField(blank=True, null=True)
     avatar = models.ImageField(upload_to=users_avatars_path, blank=True, null=True)
-    email = models.EmailField(gettext_lazy('email address'), unique=True, max_length=256, error_messages={
-        "unique": gettext_lazy('A user with that email already exists')
+    email = models.EmailField(_('email address'), unique=True, max_length=256, error_messages={
+        "unique": _('A user with that email already exists')
     })
-    is_staff = models.BooleanField(gettext_lazy('staff status'), default=False,
-                                   help_text=gettext_lazy('Designates whether the user can log into this admin site'))
-    is_active = models.BooleanField(gettext_lazy('staff status'), default=True,
-                                    help_text=gettext_lazy('Designates whether this user should be treated as active.'
+    is_staff = models.BooleanField(_('staff status'), default=False,
+                                   help_text=_('Designates whether the user can log into this admin site'))
+    is_active = models.BooleanField(_('staff status'), default=True,
+                                    help_text=_('Designates whether this user should be treated as active.'
                                                            'Unselect this instead of deleting accounts'))
-    date_joined = models.DateTimeField(gettext_lazy('date joined'), auto_now_add=True)
+    date_joined = models.DateTimeField(_('date joined'), auto_now_add=True)
     objects = UserManager()
 
     EMAIL_FIELD = 'email'
@@ -46,8 +46,8 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     REQUIRED_FIELDS = ['email']
 
     class Meta:
-        verbose_name = gettext_lazy('user')
-        verbose_name_plural = gettext_lazy('users')
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
 
     def clean(self):
         super().clean()
